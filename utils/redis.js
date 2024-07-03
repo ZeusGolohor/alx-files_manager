ccc331c9fc72f79384ac190348cbfab370ab0b7a
golohorzeus <golohorzeusufuoma@gmail.com>
2024-07-02 21:28:19 -0700

    Inside the folder utils, create a file redis.js that contains the class RedisClient.
    RedisClient should have:
    
    the constructor that creates a client to Redis:
    any error of the redis client must be displayed in the console (you should use on('error') of the redis client)
    a function isAlive that returns true when the connection to Redis is a success otherwise, false
    an asynchronous function get that takes a string key as argument and returns the Redis value stored for this key
    an asynchronous function set that takes a string key, a value and a duration in second as arguments to store it in Redis (with an expiration set by the duration argument)
    an asynchronous function del that takes a string key as argument and remove the value in Redis for this key
    After the class definition, create and export an instance of RedisClient called redisClient.

--- utils/redis.js
+++ utils/redis.js
@@ -1,6 +1,12 @@
 import { createClient } from 'redis'
 
+/**
+ * A redis client class.
+ */
 class RedisClient {
+  /**
+   * Redis class constructor
+   */
   constructor () {
     this.client = createClient()
     this.isClientConnected = true
@@ -11,10 +17,16 @@ class RedisClient {
     })
   }
 
+  /**
+   * A method to check if redis is connected.
+   */
   isAlive () {
     return this.isClientConnected
   }
 
+  /**
+   * A method used to retrieve a key from redis.
+   */
   async get (key) {
     return new Promise((resolve, reject) => {
       this.client.get(key, (err, reply) => {
@@ -27,6 +39,9 @@ class RedisClient {
     })
   }
 
+  /**
+   * A method to set a value for a key in redis.
+   */
   async set (key, value, exp) {
     return new Promise((resolve, reject) => {
       this.client.set(key, value, 'EX', exp, (err, reply) => {
@@ -39,6 +54,9 @@ class RedisClient {
     })
   }
 
+  /**
+   * A method used to delete a key and value from redis.
+   */
   async del (key) {
     return new Promise((resolve, reject) => {
       this.client.del(key, (err, reply) => {
