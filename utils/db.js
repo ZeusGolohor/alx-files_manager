882ad7a87a2c0b1c21b66e8547cdd742feb9e07c
zeusgolohor <zeusgolohor@gmail.com>
2024-07-13 18:13:24 -0700

    Refactor

--- utils/db.js
+++ utils/db.js
@@ -8,19 +8,23 @@ class DBClient {
    * DBClient class constructor
    */
   constructor () {
-    this.host = process.env.DB_HOST || 'localhost';
-    this.port = process.env.DB_PORT || 27017;
-    this.database = process.env.DB_DATABASE || 'files_manager';
-
-    const url = `mongodb://${this.host}:${this.port}/${this.database}`;
-    const client = new MongoClient(url);
+    const HOST = process.env.DB_HOST || 'localhost';
+    const PORT = process.env.DB_PORT || 27017;
+    const DATABASE = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${HOST}:${PORT}`;
+    this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
+    this.client.connect().then(() => {
+      this.db = this.client.db(`${DATABASE}`);
+    }).catch((err) => {
+      console.log(err);
+    });
   }
 
   /**
    * A method to check if the database is connected.
    */
   isAlive() {
-
+    return this.client.isConnected();
   }
 }
 
