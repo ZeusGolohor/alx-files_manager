001afd375041f913691a2b9b70d6ac8fa434b4d5
zeusgolohor <zeusgolohor@gmail.com>
2024-07-26 20:27:55 -0700

    Now that we have a simple API, it’s time to add users to our database.
    In the file routes/index.js, add a new endpoint:
    
    POST /users => UsersController.postNew
    Inside controllers, add a file UsersController.js that contains the new endpoint:
    
    POST /users should create a new user in DB:
    
    To create a user, you must specify an email and a password
    If the email is missing, return an error Missing email with a status code 400
    If the password is missing, return an error Missing password with a status code 400
    If the email already exists in DB, return an error Already exist with a status code 400
    The password must be stored after being hashed in SHA1
    The endpoint is returning the new user with only the email and the id (auto generated by MongoDB) with a status code 201
    The new user must be saved in the collection users:
    email: same as the value received
    password: SHA1 value of the value received

---
+++ controllers/UsersController.js
@@ -0,0 +1,38 @@
+import sha1 from 'sha1';
+import Queue from 'bull/lib/queue';
+import dbClient from '../utils/db';
+
+const userQueue = new Queue('email sending');
+
+export default class UsersController {
+  static async postNew(request, response) {
+    const userEmail = request.body ? request.body.email : null;
+    const userPassword = request.body ? request.body.password : null;
+
+    if (!userEmail) {
+      response.status(400).json({error: 'Missing email'});
+      return;
+    }
+    if (!userPassword) {
+      response.status(400).json({error: 'Missing password'});
+      return;
+    }
+
+    const existingUser = await (await dbClient.usersCollection()).findOne({email: userEmail});
+    if (existingUser) {
+      response.status(400).json({ error: 'Already exist' });
+      return;
+    }
+    const insertionInfo = await (await dbClient.usersCollection())
+      .insertOne({email: userEmail, password: sha1(userPassword)});
+    const userId = insertionInfo.insertedId.toString();
+    userQueue.add({ userId });
+    response.status(201).json({email: userEmail, id: userId});
+  }
+
+  static async getMe(req, res) {
+    const {user} = req;
+    res.status(200).json({email: user.email, id: user._id.toString()});
+  }
+}
+
