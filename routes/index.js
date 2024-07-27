41ab8c033f495bd81f03a3de3a56f15d039cbab0
zeusgolohor <zeusgolohor@gmail.com>
2024-07-27 02:25:11 -0700

    refactor

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController';
 
 const router = Router();
 
@@ -11,6 +10,6 @@ router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UserController.getMe);
-router.post('/files', FilesController.postUpload);
+router.get('/users/me', UsersController.getMe);
+
 module.exports = router;
