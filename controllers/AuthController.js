2a35835e35acf767a55fc89bf076c2aff2c0edea
zeusgolohor <zeusgolohor@gmail.com>
2024-07-28 02:22:03 +0000

    Now that we have a simple API, it’s time to add users to our database.
    In the file routes/index.js, add a new endpoint:
    
    POST /users => UsersController.postNew
    Inside controllers, add a file UsersController.js that contains the new endpoint:
    
    POST /users should create a new user in DB:
    
    To create a user, you must specify an email and a password
    If the email is missing, return an error Missing email with a status code 400
    If the password is missing, return an error Missing password with a status code 400
    If the email already exists in DB, return an error Already exist with a status code 400
    The password must be stored after being hashed in SHA1
    The endpoint is returning the new user with only the email and the id (auto generated by MongoDB) with a status code 201
    The new user must be saved in the collection users:
    email: same as the value received
    password: SHA1 value of the value received

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,18 +1,17 @@
+import { v4 as uuidv4 } from 'uuid';
 import redisClient from '../utils/redis';
-import dbClient from '../utils/db';
 
-export default class AppController {
-  static getStatus(req, res) {
-    res.status(200).json({
-      redis: redisClient.isAlive(),
-      db: dbClient.isAlive(),
-    });
+export default class AuthController {
+  static async getConnect(req, res) {
+    const { user } = req;
+    const token = uuidv4();
+    await redisClient.set(`auth_${token}`, user._id.toString(), 24 * 60 * 60);
+    res.status(200).json({ token });
   }
 
-  static getStats(req, res) {
-    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
-      .then(([usersCt, filesCt]) => {
-        res.status(200).json({ users: usersCt, files: filesCt });
-      });
+  static async getDisconnect(req, res) {
+    const token = req.headers['x-token'];
+    await redisClient.del(`auth_${token}`);
+    res.status(204).send();
   }
 }
